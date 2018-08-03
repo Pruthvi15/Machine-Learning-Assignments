48c374b485fb5226b6fd639ae3d1620d475dc134
unknown <pruthvikanta@gmail.com>
2018-08-03 16:29:34 +0530

    Cost is working

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -35,7 +35,8 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
-J = 1/m * sum((-y'*log(sigmoid(X*theta))-(1-y)'*log(1-sigmoid(X*theta))));
+h=sigmoid(X*theta);
+J = 1/m * sum((-y.*log(h))-(1-y).*log(1-h))+ (lambda/(2*m)) * sum(theta(2:length(theta)).^2);
 
 
 
